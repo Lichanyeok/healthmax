f9464278abfd8450a5a392aaf0258fc38a9d1986
DataJlee <jaek1997@gmail.com>
2022-08-22 23:04:59 +0900

    service first commit

--- src/main/java/com/healthmax/service/MemberService.java
+++ src/main/java/com/healthmax/service/MemberService.java
@@ -1,2 +1,37 @@
-package com.healthmax.service;public class MemberService {
+package com.healthmax.service;
+
+import com.healthmax.domain.Member;
+import com.healthmax.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        List<Member> memberList = memberRepository.findByEmail(member.getEmail());
+        if (!memberList.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 리스트 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
 }
