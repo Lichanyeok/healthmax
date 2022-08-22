358315106c24279dd7ce929fb1483fa0c02f49d8
DataJlee <jaek1997@gmail.com>
2022-08-22 21:13:08 +0900

    First commit(Jae)

---
+++ src/main/java/com/healthmax/HealthmaxApplication.java
@@ -0,0 +1,13 @@
+package com.healthmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthmaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthmax/HealthmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
